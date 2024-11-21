f3856820b8f2429c3993921ec1f7205195e9f1a2
DeLuccaBR <jefferdelucca@gmail.com>
2024-11-21 08:24:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Quem é o maior artilheiro da Seleção Brasileira?',
+    'O maior artilheiro da Seleção é o Neymar Da Silva Santos Junior, somando um total de 79 gols com a camisa.'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é o maior clássico do futebol mundial?',
+    'O maior clássico do futebol mundial é conhecido como "El Classico" sendo disputado por Barcelona x Real Madrid da espanha.'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é o maior clube de Ribeirão Preto',
+    'O maior clube de Ribeirão Preto com certeza é o Botafogo-SP, possuindo uma grande história no futebol nacional.'
+)
+
+criaCartao(
+    'Futebol',
+    'Na ultima edição de Libertadores, quem foi o clube que se consagrou campeão?',
+    'Na ultima edição de Libertadores quem se consagrou campeão foi o Fluminense, do Rio de Janeiro contra o Boca Juniors da Argentina com gol decisivo de John Kennedy na prorrogação.'
+)
